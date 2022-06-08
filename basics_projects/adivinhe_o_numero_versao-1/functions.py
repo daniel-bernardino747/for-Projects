660dc93c7b3c05173311736a0e26026c786d0a5c
Daniel <dn.danielbernardino@gmail.com>
2022-06-08 14:26:37 -0300

    Programa finalizado (Versão 1.0)

--- basics_projects/adivinhe_o_numero_versao-1/functions.py
+++ basics_projects/adivinhe_o_numero_versao-1/functions.py
@@ -5,7 +5,7 @@ def user_answer(question, category=str):
         query = category(input(question))
         return query
     except ValueError:
-        return '\nO valor digitado não condiz com o tipo de dado que o input recebe.'
+        return ValueError
 
 def user_condition(question):
     query = str(input(question)).strip().lower()[0]
@@ -15,16 +15,15 @@ def value_random(first=0, last=9):
     try:
         num1 = int(first)
         num2 = int(last)
-        if num1 < num2:
-            value = randint(num1, num2)
-        else:
-            value = randint(num2, num1)
     except ValueError:
-        return 'Responda com um número, letras e outros algarismos não são aceitos. Tente novamente.'
+        return ValueError
+    if num1 < num2:
+        value = randint(num1, num2)
+    else:
+        value = randint(num2, num1)
     return value
 
-
 if __name__ == '__main__':
-    # print(value_random(input('Primeiro valor: '), input('Segundo valor: ')))
-    ans = user_answer('Digite um número ', int)
-    print(ans)
+    a = value_random(input('Primeiro valor: '), input('Segundo valor: '))
+    if a == ValueError:
+        print('eerouuuu')
--- basics_projects/adivinhe_o_numero_versao-1/main.py
+++ basics_projects/adivinhe_o_numero_versao-1/main.py
@@ -1,15 +1,68 @@
 import functions
+import os
+from time import sleep
 
 # Create a function that draws a value between 0 and 9
 
-# If the user wants, he can change these values
-ans = functions.user_condition('Deseja escolher entre quais números quer adivinhar? [Sim / Não] ')
-if ans == 's':
-    value = functions.value_random(input('Primeiro valor: '), input('Último valor: '))
-elif ans == 'n':
-    value = functions.value_random()
-else:
-    print('Não consegui entender. Por favor, responda com "Sim" ou "Não".')
-print(value)
-# Ask what value the user thinks it is
-# Ask if you want to play again
+while True:
+    # If the user wants, he can change these values
+
+    while True:
+        try:
+            condition = functions.user_condition('\nDeseja escolher entre quais números quer adivinhar? [Sim / Não] ')
+            if condition == 's':
+                value = functions.value_random(input('Primeiro valor: '), input('Último valor: '))
+                if value == ValueError:
+                    print('Responda com um número. Letras e/ou outros algarismos não são aceitos. Tente novamente.')
+                else:
+                    break 
+            elif condition == 'n':
+                value = functions.value_random()
+                os.system('clear')
+                break
+            else:
+                print('Não consegui entender. Por favor, responda com "Sim" ou "Não".')
+        except IndexError:
+            print('Você não digitou nada. Tente novamente.')
+    if condition == 'n':
+        print('\n[Por padrão, o computador sorteou um valor entre 0 e 9.]')
+    else:
+        pass
+
+    while True:
+        # Ask what value the user thinks it is
+
+        answer = functions.user_answer('Qual valor você acha que o computador sorteou? ', int)
+        if answer == value:
+            os.system('clear')
+            print(f'Você acertou, o computador pensou no número {value}')
+            sleep(4)
+            os.system('clear')
+            break
+        elif answer != ValueError:
+            print(f'\nNão foi dessa vez, seu número foi {answer} e o número do computador foi {value}.')
+            sleep(4)
+            os.system('clear')
+            break
+        elif answer == ValueError:
+            print('Você não digitou um número. Tente novamente.')
+
+    while True:
+        # Ask if the user want to play again
+
+        try:
+            game_again = functions.user_condition('Quer jogar de novo? [Sim / Não] ')
+            if game_again == 's':
+                break
+            elif game_again == 'n':
+                break
+            else:
+                print('Não consegui entender. Por favor, responda com "Sim" ou "Não".')
+        except IndexError:
+            print('Você não digitou nada. Tente de novo.')
+            os.system('clear')
+        os.system('clear')
+    if game_again == 'n':
+        break
+
+print('\n== Programa Encerrado. ==\n')
