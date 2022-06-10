7c0736e154a011729b74aa002bd106eb37798eee
Daniel <dn.danielbernardino@gmail.com>
2022-06-10 17:02:45 -0300

    completed tests

---
+++ basics_projects/criador_de_qrcode_versao-1/main.py
@@ -0,0 +1,112 @@
+import qrcode
+# teste1
+
+"""
+# teste1
+img = qrcode.make('Eba tá funcionando')
+type(img)  # qrcode.image.pil.PilImage
+img.save("primeiro_qrcode.png")
+"""
+
+"""
+# teste2
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data('Some data')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save('teste1.png')
+"""
+
+"""
+# teste3
+qr = qrcode.QRCode(
+
+    version=2,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data('Some data')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save('teste2.png')
+"""
+
+"""
+# teste3
+qr = qrcode.QRCode(
+    version=10,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data('Some data')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save('teste3.png')
+"""
+
+"""
+# teste4
+qr = qrcode.QRCode(
+    version=40,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data('Some data')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save('teste4.png')
+"""
+
+"""
+# teste5
+qr = qrcode.QRCode(
+    version=2,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=100,
+    border=4,
+)
+qr.add_data('Some data')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save('teste5.png')
+"""
+"""
+# teste6
+qr = qrcode.QRCode(
+    version=2,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=100,
+    border=4,
+)
+qr.add_data('Some data')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="white", back_color="black")
+img.save('teste6.png')
+"""
+
+# teste7
+qr = qrcode.QRCode(
+    version=2,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=100,
+    border=4,
+)
+qr.add_data('Some data')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color=(255, 195, 235), back_color=(55, 95, 35))
+img.save('teste7.png')
