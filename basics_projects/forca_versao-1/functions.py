e98d07379ff36aa801330de3d67b31ea5236c7b5
Daniel <dn.danielbernardino@gmail.com>
2022-06-10 10:34:10 -0300

    used letter dont repeat

--- basics_projects/forca_versao-1/functions.py
+++ basics_projects/forca_versao-1/functions.py
@@ -16,19 +16,24 @@ def hangman(word):
             print(f'\n\nLetras usadas:', end=' ')
             for m in used_letters:
                 print(m, end= ' ')
-        user_letter = str(input('\nDigite uma letra: ')).upper()
+        while True:
+            user_letter = str(input('\nDigite uma letra: ')).upper()
+            if user_letter in used_letters:
+                print(f'Você já adicionou a letra {user_letter}, tente outra.')
+            else:
+                break
         used_letters.append(user_letter)
         if user_letter in word:
             unnamed = [user_letter if value == user_letter else unnamed[index] for index, value in enumerate(normal_letters)]
             letters.remove(user_letter)
             system('clear')
-            print('\nVocê achou uma letra.')
+            print('\nVocê achou uma letra da palavra.')
         elif user_letter == '0':
             break
         else:
             lives -= 1
             system('clear')
-            print('Errou')
+            print(f'A letra {user_letter} não está na palavra, perdeu uma vida.')
         if lives == 0:
             system('clear')
             print(f'Você perdeu todas as suas vidas. A palavra era: {word}')
