610488d5785106cbdec8628abe4c03ab5874719d
Daniel <dn.danielbernardino@gmail.com>
2022-06-10 10:26:02 -0300

    show used letters

--- basics_projects/forca_versao-1/functions.py
+++ basics_projects/forca_versao-1/functions.py
@@ -3,6 +3,7 @@ from os import system
 def hangman(word):
     system('clear')
     lives = 6
+    used_letters = list()
     letters = set(word)
     normal_letters = list(word)
     unnamed = [i.replace(f'{i}', '_') for i in word]
@@ -11,13 +12,13 @@ def hangman(word):
         print(f'Você tem {lives} vidas.\n')
         for a in unnamed:
             print(a, end=' ')
+        if not used_letters == list():
+            print(f'\n\nLetras usadas:', end=' ')
+            for m in used_letters:
+                print(m, end= ' ')
         user_letter = str(input('\nDigite uma letra: ')).upper()
+        used_letters.append(user_letter)
         if user_letter in word:
-            """for index, value in enumerate(normal_letters):
-                if user_letter == value:
-                    unnamed[index] = user_letter
-                else:
-                    unnamed[index] = unnamed[index]"""
             unnamed = [user_letter if value == user_letter else unnamed[index] for index, value in enumerate(normal_letters)]
             letters.remove(user_letter)
             system('clear')
