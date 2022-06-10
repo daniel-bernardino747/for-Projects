933796a619c06ef679eb3578a85e78b5ef1e23e7
Daniel <dn.danielbernardino@gmail.com>
2022-06-10 10:13:42 -0300

    add lives in game

--- basics_projects/forca_versao-1/functions.py
+++ basics_projects/forca_versao-1/functions.py
@@ -1,9 +1,13 @@
+from os import system
+
 def hangman(word):
+    lives = 6
     letters = set(word)
     normal_letters = list(word)
     unnamed = [i.replace(f'{i}', '_') for i in word]
     print('Adivinhe a palavra abaixo:')
     while True:
+        print(f'Você tem {lives} vidas.\n')
         for a in unnamed:
             print(a, end=' ')
         user_letter = str(input('\nDigite uma letra: ')).upper()
@@ -15,11 +19,18 @@ def hangman(word):
                     unnamed[index] = unnamed[index]"""
             unnamed = [user_letter if value == user_letter else unnamed[index] for index, value in enumerate(normal_letters)]
             letters.remove(user_letter)
+            system('clear')
             print('\nVocê achou uma letra.')
         elif user_letter == '0':
             break
         else:
+            lives -= 1
+            system('clear')
             print('Errou')
+        if lives == 0:
+            system('clear')
+            print(f'Você perdeu todas as suas vidas. A palavra era: {word}')
+            break
 
 if __name__ == '__main__':
     word = 'blabla'
