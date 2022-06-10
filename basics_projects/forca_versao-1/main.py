c55a71fdcee727d8efcdd835e603c4fb75fe3c91
Daniel <dn.danielbernardino@gmail.com>
2022-06-10 00:48:28 -0300

    create function hangman

--- basics_projects/forca_versao-1/main.py
+++ basics_projects/forca_versao-1/main.py
@@ -1,5 +1,8 @@
 from random import choice
+from functions import hangman
 
 with open(r'C:\Users\Daniel.DESKTOPDAN\principal_tools\Documents\For_Python\basics_projects\forca_versao-1\Lista-de-Palavras.txt', 'r') as arquivo:
     arq = arquivo.readlines()
-print(choice(arq))
+
+word = choice(arq).strip('\n').upper()
+hangman(word)
