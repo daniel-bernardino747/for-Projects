bcc01db50b78aa742942f13da33fdf47b1c58d91
Daniel <dn.danielbernardino@gmail.com>
2022-06-10 12:47:13 -0300

    clear project

--- basics_projects/forca_versao-1/main.py
+++ basics_projects/forca_versao-1/main.py
@@ -7,18 +7,16 @@ file = join(dirname(__file__), 'Lista-de-Palavras.txt')
 with open(file, 'r') as f:
     arq = f.readlines()
 
-while True:
+exit = 'E'
+while exit not in 'n':
     word = choice(arq).strip('\n').upper()
     hangman(word)
-    exit = 'E'
-    while exit not in 'sn':
-        exit = user_condition(f'{"=" * 30} \nVamos brincar de novo? [Sim / Não] ', str)
-        if exit == 's':
-            break
-        elif exit == 'n':
-            break
-        print('Eu não entendi. Responda com "Sim" ou "Não".')
-    if exit == 'n':
+    exit = user_condition(f'{"=" * 30} \nVamos brincar de novo? [Sim / Não] ', str)
+    if exit == 's':
+        pass
+    elif exit == 'n':
         break
+    else:
+        print('Eu não entendi. Responda com "Sim" ou "Não".')
 system('clear')
 print('\n== Programa Encerrado ==')
