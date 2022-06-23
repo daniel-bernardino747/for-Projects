b9b60b35039ceb63181c4b96c0c4a0b5fdd693c6
Daniel <dn.danielbernardino@gmail.com>
2022-06-23 20:32:54 -0300

    created solver sudoku

--- basics_projects/9_resolved_sudoku/main.py
+++ basics_projects/9_resolved_sudoku/main.py
@@ -47,12 +47,17 @@ def solver_sudoku(puzzle):
 
         # step 1.2.1: confere se esse chute é válido
         if is_valid(puzzle, guess, row, col):
-            # step 1.2.1.1: cria variáveis com as linhas e colunas
-            # step 1.2.1.2: Se o chute estiver lá, retorna que n é válido
-            # step 1.2.1.3: cria os quadros e testa eles
-            # step 1.2.1.4: se passar tudo isso, é válido
-        # step 1.2.2: se for: muda a posição para seu chute
+            # step 1.2.2: se for: muda a posição para seu chute
+            puzzle[row][col] = guess
+
             # step  1.2.2.1: retorna True
+            if solver_sudoku(puzzle):
+                return True
+        
         # step 1.2.3: se não for: deixa -1
+        puzzle[row][col] = -1
+    
     # step 1.3: Se não deu returna False
+    return False
+
 # step 2: Testa com __name__ = __main__
